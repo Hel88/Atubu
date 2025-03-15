b41c336c95fb641959889880a34a16aa583da134
library-rat <1ucas_z@protonmail.com>
2025-03-15 14:03:46 -0400

    fist test storing and accessing

--- app/src/main/java/com/example/atubu/MainActivity.kt
+++ app/src/main/java/com/example/atubu/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.atubu
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var dataAccessObject: DataAccessObject
+    private lateinit var helloView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +22,10 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        dataAccessObject = DataAccessObject(applicationContext)
+        helloView = findViewById(R.id.HelloDisplay)
+        helloView.text = dataAccessObject.incrementTest()
+
     }
 }
