4ce08b7fdc4ff74d6296bacd486cfa53103c5e25
library-rat <1ucas_z@protonmail.com>
2025-03-28 21:43:01 -0400

    test passed

--- app/src/main/java/com/example/atubu/MainActivity.kt
+++ app/src/main/java/com/example/atubu/MainActivity.kt
@@ -7,8 +7,10 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.atubu.dataInterface.DataAccessObject
+import com.example.atubu.dataInterface.Day
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import java.sql.Date
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,9 +30,24 @@ class MainActivity : AppCompatActivity() {
 
         dataAccessObject = DataAccessObject.getDAO(applicationContext)
         helloView = findViewById(R.id.HelloDisplay)
-        GlobalScope.launch {
-        helloView.text = dataAccessObject.roomTest()
+        dataAccessObject.getDaysBetween(Date(Long.MIN_VALUE), Date(Long.MAX_VALUE)){
+            result ->
+            result.onSuccess{
+                val obtained = result.getOrNull()
+                if (obtained is List<Day>){
 
+                helloView.text = listToString(obtained)
+                }
+            }
+            result.onFailure {
+                helloView.text = "Oopsie somthing went wrong"
+            }
+        }
+    }
+
+    fun listToString(lst : List<Day>) : String{
+        return lst.joinToString(separator = "\n") { day ->
+            "Date: ${day.date}, Drunk: ${day.waterDrunkL}, State: ${day.plantState}"
         }
     }
 }
